1873d1b1c90d5e354ff2b11a0f8594789491c9a4
ryosyntaxx <riodwi12174@gmail.com>
2024-01-23 16:09:11 +0700

    i think service is complete

--- uptodown/uptodown/items.py
+++ uptodown/uptodown/items.py
@@ -10,6 +10,7 @@ class UptodownItem(scrapy.Item):
     # define the fields for your item here like:
     link = scrapy.Field()
     tag = scrapy.Field()
+    id = scrapy.Field()
     crawling_time = scrapy.Field()
     crawling_time_epoch = scrapy.Field()
     path_data_raw = scrapy.Field()
@@ -22,52 +23,3 @@ class UptodownItem(scrapy.Item):
     reviews_rating = scrapy.Field()
     detail_application = scrapy.Field()
     ...
-
-
-"""
-{
-    "link": "string",
-    "domain": "string",
-    "tag": ["string"],
-    "crawling_time": "yyyy-MM-dd HH:mm:ss",
-    "crawling_time_epoch": "epochmillis",
-    "path_data_raw": "string",
-    "path_data_clean": "string",
-    "reviews_name": "string",    
-    "location_reviews": "string",
-    "category_reviews": "string",
-    "total_reviews": "integer",
-    "reviews_rating": {
-      "total_rating": "integer",
-      "detail_total_rating": [{
-          "score_rating": "integer",
-          "category_rating": "string"
-      }]
-    },
-    "detail_reviews": {
-      "username_reviews": "string",
-      "image_reviews": "string",
-      "created_time": "yyyy-MM-dd HH:mm:ss",
-      "created_time_epoch":"epochmillis",
-      "email_reviews": "string",
-      "company_name": "string",
-      "location_reviews": "string",
-      "title_detail_reviews": "string",
-      "reviews_rating": "integer",
-      "detail_reviews_rating": [{
-          "score_rating": "integer",
-          "category_rating": "string"
-      }],
-      "total_likes_reviews": "integer",
-      "total_dislikes_reviews": "integer",
-      "total_reply_reviews": "integer",
-      "content_reviews": "string",
-      "reply_content_reviews": {
-          "username_reply_reviews": "string",
-          "content_reviews": "string"
-      },
-      "date_of_experience":"yyyy-MM-dd HH:mm:ss",
-      "date_of_experience_epoch":"epochmillis"
-  }
-}
-"""
--- uptodown/uptodown/spiders/service.py
+++ uptodown/uptodown/spiders/service.py
@@ -12,7 +12,6 @@ from uptodown.items import UptodownItem
 
 class ServiceSpider(scrapy.Spider):
     name = "service"
-    # allowed_domains = ["id.uptodown.com"]
     start_urls = ["https://id.uptodown.com"]
     
     _platforms = ['andorid', 'windows', 'mac']
@@ -34,11 +33,28 @@ class ServiceSpider(scrapy.Spider):
             yield Request(url=type, callback=self.__collect_apps)
         ...
     
+    
     def __collect_apps(self, response: Response) -> List[str]:
         for app in response.css('div[class="name"] > a ::attr(href)').getall():
             yield Request(url=app, callback=self.__parser_app)
         ...
 
+
+    def __extract_url(self, url: str) -> dict:
+        pieces = url.split('/')
+        return {
+            "platform": pieces.pop(),
+            "url": '/'.join(pieces)
+        }
+    
+
+    def __strip(self, text: str) -> str:
+        try:
+            return text.strip()
+        except Exception:
+            return text
+        ...
+    
     def __parser_app(self, response: Response) -> dict:
         body: Response = response.css('div.c1')
 
@@ -48,12 +64,13 @@ class ServiceSpider(scrapy.Spider):
             "id": body.css('#detail-app-name ::attr(code)').get(),
             "title": body.css('#detail-app-name ::text').get(),
             "information": body.css('div.detail > h2 ::text').get(),
-            "url": response.url,
+            "url": self.__extract_url(response.url)["url"],
+            "platform": self.__extract_url(response.url)["platform"],
             "version": body.css('div.version ::text').get(),
-            "author": body.css('div.autor a ::text'),
+            "author": self.__strip(body.css('div.autor a ::text').get()),
             "descriptions": body.css('div.text-description p::text').extract(),
-            "total_reviews": body.css('#more-comments-rate-section ::text').extract()[-1].strip(),
-            "ratings": body.css('#rating ::text').get().strip(),
+            "total_reviews": body.css('#more-comments-rate-section ::text').extract()[-1].strip() if body.css('#more-comments-rate-section') else None,
+            "ratings": body.css('#rating ::text').get().strip() if body.css('#rating') else None,
             "technical-information": {
                 key.css('td:nth-child(2) ::text').get().strip(): key.css('td:last-child ::text').get().strip() for key in body.css('#technical-information tr')
             },
@@ -68,36 +85,95 @@ class ServiceSpider(scrapy.Spider):
 
         yield Request(url=f'{header["url"]}/mng/v2/app/{header["id"]}/comments', callback=self.__extract_review, cb_kwargs=header)
 
-    def __extract_review(self, response: Response, **kwargs) -> List[dict]:
+    def __extract_review(self, response: Response, **header) -> List[dict]:
         reviews = Selector(text=response.json()['content'])
 
-        ic(kwargs)
+        self.offset = 10
+        self.temporarys: List[dict] = []
 
         for review in reviews.css('div.comment'):
-            yield {
-                "username": review.css('span.user ::text'),
-                "avatar": review.css('img')[0].attrib('src'),
-                "posted": review.css('span')[0].get(),
+            self.temporarys.append({
+                "username": review.css('span.user ::text').get().strip(),
+                "avatar": review.css('img')[0].attrib['src'],
+                "posted": review.css('span:nth-child(3) ::text').get().strip(),
                 "ratings": len(review.css('img.active')),
-                "likes": review.css('div.favs-icon ::text'),
-                "comment": review.css('p ::text')
-            }
+                "likes": review.css('div[name="favs-icon"] span::text').get() if not "Like" else None,
+                "comment": review.css('p ::text').extract()
+            })
 
+        ic("masuk extract review")
+        yield Request(url=f'{header["url"]}/mng/v2/app/{header["id"]}/comments/unixtime?offset={self.offset}', callback=self.__exctract_review_api, cb_kwargs=dict(header = header), errback=self.__finally)
         ...
 
-    def __exctract_review_api(self, response: Response):
+    def __exctract_review_api(self, response: Response, header):
         reviews = response.json()
+        self.offset+=10
 
         if reviews["success"]:
             for review in reviews["data"]:
-                yield {
+                self.temporarys.append({
                     "username": review["userName"],
                     "avatar": review["icon"],
                     "posted": review["timeAgo"],
                     "ratings": review["rating"],
                     "likes": review["likes"],
                     "comment": review["text"]
-                }
-        else:
-            yield False
+                })
+
+            yield Request(url=f'{header["url"]}/mng/v2/app/{header["id"]}/comments/unixtime?offset={self.offset}', callback=self.__exctract_review_api, cb_kwargs=dict(header = header), errback=self.__finally)
         ...
+
+    def __finally(self, failure):
+        ic('masuk finaly')
+
+        ic(len(self.temporarys))
+        for result in self.temporarys:
+            items = UptodownItem()
+
+            items["link"] = failure.request.cb_kwargs["header"]["url"]
+            items["id"] = failure.request.cb_kwargs["header"]["id"]
+            items["tag"] = ["id.uptodown.com"]
+            items["crawling_time"] = strftime('%Y-%m-%d %H:%M:%S')
+            items["crawling_time_epoch"] = int(time())
+            items["path_data_raw"] = ""
+            items["path_data_clean"] = ""
+            items["reviews_name"] = failure.request.cb_kwargs["header"]["title"]
+            items["location_reviews"] = None
+            items["category_reviews"] = "" # Bro?
+            items["total_reviews"] = failure.request.cb_kwargs["header"]["total_reviews"]
+            items["reviews_rating"] = {
+                "total_rating": failure.request.cb_kwargs["header"]["ratings"],
+                "detail_total_rating": None
+            }
+            items["detail_reviews"] = {
+                "username_reviews": result["username"],
+                "image_reviews": result["avatar"],
+                "created_time": result["posted"],
+                "created_time_epoch": None,
+                "email_reviews": None,
+                "company_name": None,
+                "location_reviews": None,
+                "title_detail_reviews": None,
+                "rating_given": len(self.temporarys()),
+                "reviews_rating": result["ratings"],
+                "detail_reviews_rating": None,
+                "total_likes_reviews": result["likes"],
+                "total_dislikes_reviews": None,
+                "total_reply_reviews": "", # Bro?
+                "content_reviews": result["comment"],
+                "reply_content_reviews": "",
+                "date_of_experience": result["posted"],
+                "date_of_experience_epoch": None
+            }
+            items["detail_application"] = {
+                "title": failure.request.cb_kwargs["header"]["title"],
+                "information": failure.request.cb_kwargs["header"]["information"],
+                "platform": failure.request.cb_kwargs["header"]["platform"],
+                "version": failure.request.cb_kwargs["header"]["version"],
+                "author": failure.request.cb_kwargs["header"]["author"],
+                "descriptions": failure.request.cb_kwargs["header"]["descriptions"],
+                "technical-information": failure.request.cb_kwargs["header"]["technical-information"],
+                "previous_version": failure.request.cb_kwargs["header"]["previous_version"]
+            }
+
+            yield items
